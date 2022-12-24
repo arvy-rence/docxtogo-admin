d525c27fa01284b3898e0a91285f026ce449c45e
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-25 01:15:59 +0800

    [UPDATE] / Remove image and replaced with logout button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Navbar, Dropdown, Avatar} from "flowbite-react";
+import {Navbar, Button} from "flowbite-react";
 import Link from "next/link";
 
 export const LoggedInNavbar = ({activePage}) => {
@@ -14,33 +14,9 @@ export const LoggedInNavbar = ({activePage}) => {
         </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Dropdown
-          arrowIcon={false}
-          inline={true}
-          label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true}/>}
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">
-              Bonnie Green
-            </span>
-            <span className="block truncate text-sm font-medium">
-              name@flowbite.com
-            </span>
-          </Dropdown.Header>
-          <Dropdown.Item>
-            Dashboard
-          </Dropdown.Item>
-          <Dropdown.Item>
-            Settings
-          </Dropdown.Item>
-          <Dropdown.Item>
-            Earnings
-          </Dropdown.Item>
-          <Dropdown.Divider/>
-          <Dropdown.Item>
-            Sign out
-          </Dropdown.Item>
-        </Dropdown>
+        <Button className="text-white font-work font-bold">
+          <Link href="/">LOG OUT</Link>
+        </Button>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
