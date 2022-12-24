b0a35c5d4868df525c6d6aba38a60f9ec71ae791
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-25 00:12:08 +0800

    [ADD] / Add pages and Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -45,23 +45,23 @@ export const LoggedInNavbar = ({activePage}) => {
       </div>
       <Navbar.Collapse>
         <Link href="/dashboard"
-                     className={`font-work ${activePage === 'dashboard' ? 'text-white' : 'text-[#538ed8]'} text-lg`}>
+              className={`font-work ${activePage === 'dashboard' ? 'text-white' : 'text-[#538ed8]'} text-lg`}>
           Dashboard
         </Link>
         <Link href="/dashboard/class-list"
-                     className={`font-work ${activePage === 'class-list' ? 'text-white' : 'text-[#538ed8]'} text-lg`}>
+              className={`font-work ${activePage === 'class-list' ? 'text-white' : 'text-[#538ed8]'} text-lg`}>
           List of Classes
         </Link>
         <Link href="/dashboard/requests"
-                     className={`font-work ${activePage === 'requests' ? 'text-white' : 'text-[#538ed8]'} text-lg`}>
+              className={`font-work ${activePage === 'requests' ? 'text-white' : 'text-[#538ed8]'} text-lg`}>
           Requests
         </Link>
         <Link href="/dashboard/logs"
-                     className={`font-work ${activePage === 'logs' ? 'text-white' : 'text-[#538ed8]'} text-lg`}>
+              className={`font-work ${activePage === 'logs' ? 'text-white' : 'text-[#538ed8]'} text-lg`}>
           Logs
         </Link>
         <Link href="/dashboard/reports"
-                     className={`font-work ${activePage === 'reports' ? 'text-white' : 'text-[#538ed8]'} text-lg`}>
+              className={`font-work ${activePage === 'reports' ? 'text-white' : 'text-[#538ed8]'} text-lg`}>
           Reports
         </Link>
       </Navbar.Collapse>
