e2b0a249c1063f2593c83af809eb0c8b7fa5b20e
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-25 01:07:51 +0800

    [ADD] / Add info card and added card components to the page

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -3,14 +3,33 @@ import React, {
   useState,
 } from "react";
 import {LoggedInNavbar} from "../../components/Navbar";
+import InfoCard from "../../components/dashboard/InfoCard";
+import {FaFolder, FaFileSignature, FaCheckSquare, FaCalendarDay} from "react-icons/fa";
 
-const Dashboard = () => {
+const Dashboard = (props) => {
   return (
     <>
       <LoggedInNavbar activePage="dashboard"/>
-      <h1>Dashboard</h1>
+      <div className="flex justify-center items-center">
+        <InfoCard value={('000' + props.onProcess).slice(-4)} icon={<FaFolder/>} title={"On Process"}/>
+        <InfoCard value={('000' + props.forSignature).slice(-4)} icon={<FaFileSignature/>} title={"For Signature"}/>
+        <InfoCard value={('000' + props.forRelease).slice(-4)} icon={<FaCheckSquare/>} title={"For Release"}/>
+        <InfoCard value={('000' + props.toReceive).slice(-4)} icon={<FaCalendarDay/>} title={"To Receive"}/>
+      </div>
     </>
   )
+}
+
+export async function getStaticProps() {
+  // TODO replace with API call
+  return {
+    props: {
+      onProcess: 1,
+      forSignature: 2,
+      forRelease: 5,
+      toReceive: 2,
+    },
+  };
 }
 
 export default Dashboard
