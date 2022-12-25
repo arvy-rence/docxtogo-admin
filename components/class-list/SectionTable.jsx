6a2580ce59e6b27129999f0c9e884a19dca9da20
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-26 00:06:56 +0800

    [UPDATE] / Add styling and link to the SectionTable

--- components/class-list/SectionTable.jsx
+++ components/class-list/SectionTable.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import {Table} from "flowbite-react";
+import {Button, Table} from "flowbite-react";
 
 export const SectionTable = ({sections}) => {
   const tableHeaders = ["Section", "Adviser", "Strand", "Room Assignment", "Action"];
   return (
-    <Table>
-      <Table.Head>
+    <Table striped={true}>
+      <Table.Head className="bg-primary text-white">
         {tableHeaders.map((header, index) => (
           <Table.HeadCell key={index}>{header}</Table.HeadCell>
         ))}
       </Table.Head>
-      <Table.Body>
+      <Table.Body className="divide-y">
         {sections.map((item, index) => (
           <Table.Row key={index}>
-            <Table.Cell>{item.section}</Table.Cell>
-            <Table.Cell>{item.adviser}</Table.Cell>
-            <Table.Cell>{item.strand}</Table.Cell>
-            <Table.Cell>{item.roomAssignment}</Table.Cell>
-            <Table.Cell>Open Record</Table.Cell>
+            <Table.Cell className="text-primary font-bold">{item.section}</Table.Cell>
+            <Table.Cell className="text-black">{item.adviser}</Table.Cell>
+            <Table.Cell className="text-black">{item.strand}</Table.Cell>
+            <Table.Cell className="text-black">{item.roomAssignment}</Table.Cell>
+            <Table.Cell>
+              <Button className="h-[2rem] uppercase font-bold" href={`/dashboard/class-list/${item.section}`}>
+                Open Record
+              </Button>
+            </Table.Cell>
           </Table.Row>
         ))}
       </Table.Body>
