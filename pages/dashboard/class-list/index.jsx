cfd75ee95c9ac1847dd8f5181dee1ff75a404563
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-25 20:55:53 +0800

    [ADD] / Move data to static props function

--- pages/dashboard/class-list/index.jsx
+++ pages/dashboard/class-list/index.jsx
@@ -6,49 +6,7 @@ import {LoggedInNavbar} from "../../../components/Navbar";
 import {Tabs} from "flowbite-react";
 import {SectionTable} from "../../../components/class-list/SectionTable";
 
-const ClassList = () => {
-  const grade11 = [
-    {
-      section: "11-A",
-      adviser: "Mr. John Doe",
-      strand: "STEM",
-      roomAssignment: "REX 302"
-    },
-    {
-      section: "11-B",
-      adviser: "Mr. John Doe",
-      strand: "ABM",
-      roomAssignment: "WIN 102"
-    },
-    {
-      section: "11-C",
-      adviser: "Mr. John Doe",
-      strand: "HUMSS",
-      roomAssignment: "REX 202"
-    }
-  ]
-
-  const grade12 = [
-    {
-      section: "12-A",
-      adviser: "Mr. John Doe",
-      strand: "STEM",
-      roomAssignment: "WES 202"
-    },
-    {
-      section: "12-B",
-      adviser: "Mr. John Doe",
-      strand: "ABM",
-      roomAssignment: "WIN 202"
-    },
-    {
-      section: "12-C",
-      adviser: "Mr. John Doe",
-      strand: "HUMSS",
-      roomAssignment: "REX 202"
-    }
-  ]
-
+const ClassList = ({sectionList}) => {
   return (
     <>
       <LoggedInNavbar activePage="class-list"/>
@@ -61,14 +19,65 @@ const ClassList = () => {
           title="Grade 11"
           active={true}
         >
-          <SectionTable sections={grade11}/>
+          <SectionTable sections={sectionList.grade11}/>
         </Tabs.Item>
         <Tabs.Item title="Grade 12">
-          <SectionTable sections={grade12}/>
+          <SectionTable sections={sectionList.grade12}/>
         </Tabs.Item>
       </Tabs.Group>
     </>
   )
+}
+
+export async function getStaticProps() {
+  // TODO fetch section list make sure to use the structure {grade11: [{...}], grade12: [{...}]}
+  const sections = {
+    grade11: [
+      {
+        section: "11-A",
+        adviser: "Mr. John Doe",
+        strand: "STEM",
+        roomAssignment: "REX 302"
+      },
+      {
+        section: "11-B",
+        adviser: "Mr. John Doe",
+        strand: "ABM",
+        roomAssignment: "WIN 102"
+      },
+      {
+        section: "11-C",
+        adviser: "Mr. John Doe",
+        strand: "HUMSS",
+        roomAssignment: "REX 202"
+      }
+    ],
+    grade12: [
+      {
+        section: "12-A",
+        adviser: "Mr. John Doe",
+        strand: "STEM",
+        roomAssignment: "WES 202"
+      },
+      {
+        section: "12-B",
+        adviser: "Mr. John Doe",
+        strand: "ABM",
+        roomAssignment: "WIN 202"
+      },
+      {
+        section: "12-C",
+        adviser: "Mr. John Doe",
+        strand: "HUMSS",
+        roomAssignment: "REX 202"
+      }
+    ]
+  }
+  return {
+    props: {
+      sectionList: sections
+    },
+  };
 }
 
 export default ClassList
