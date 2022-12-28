ee55d4b1615e096f4200650f0c98f8da4390ce0a
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-28 20:47:45 +0800

    [ADD] / Add print button and logic for adding files

--- pages/dashboard/class-list/[section]/[lrn]/index.jsx
+++ pages/dashboard/class-list/[section]/[lrn]/index.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import {LoggedInNavbar} from "../../../../../components/Navbar";
 import {Loader} from "../../../../../components/Loader";
+import {Label, TextInput, Button, Checkbox, FileInput, ListGroup} from "flowbite-react";
+import {BsFillFileEarmarkTextFill} from "react-icons/bs";
+import EditStudentForm from "../../../../../components/studentPage/EditStudentForm";
 
 /**
  * Dynamic page to render a student's information
  * @param lrn LRN of the student which will be used to fetch the student's information
+ * @param studentInfo Object containing the student's information
  * @returns {JSX.Element}
  * @constructor
  */
-export const StudentPage = ({lrn}) => {
+export const StudentPage = ({lrn, studentInfo}) => {
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  function handleFileSelect(e) {
+    const files = Array.prototype.slice.call(e.target.files)
+    setUploadedFiles(files);
+  }
+
+  function handlePrint(e) {
+    e.preventDefault();
+    // print selected files
+    print()
+  }
+
   return (
     <>
       <LoggedInNavbar activePage="class-list"/>
@@ -16,8 +33,51 @@ export const StudentPage = ({lrn}) => {
         <>
           <div className="mx-[5%] my-[2rem]">
             <h1 className="font-work text-3xl font-bold text-primary">
-              Student Page {lrn}
+              Student Page of
             </h1>
+            <div className="flex gap-[10rem] font-work">
+              <EditStudentForm/>
+              <div id="fileUpload" className="w-1/2">
+                <div className="mb-2 block">
+                  <Label
+                    htmlFor="file"
+                    className="font-work text-primary text-xl font-bold"
+                    value="Select files for printing"
+                  />
+                </div>
+                <FileInput
+                  id="file"
+                  multiple={true}
+                  onChange={handleFileSelect}
+                  helperText="Upload related files to print it"
+                />
+                <div className="mt-8">
+                  <h3 className="font-work text-xl font-bold text-primary">
+                    Selected files
+                  </h3>
+                  <ListGroup>
+                    {(uploadedFiles.length > 0) ?
+                      uploadedFiles.map((file, index) => (
+                        <ListGroup.Item key={index} icon={BsFillFileEarmarkTextFill}>
+                          <div className="flex justify-between">
+                            {file.name}{'  '}
+                          </div>
+                        </ListGroup.Item>
+                      )) : (
+                        <ListGroup.Item icon={BsFillFileEarmarkTextFill}>
+                          <div className="flex justify-between">
+                            No files selected
+                          </div>
+                        </ListGroup.Item>
+                      )
+                    }
+                  </ListGroup>
+                  <Button className="mt-4" onClick={handlePrint} disabled={uploadedFiles.length === 0}>
+                    Print selected files
+                  </Button>
+                </div>
+              </div>
+            </div>
           </div>
         </>
       } data={lrn}/>
@@ -34,9 +94,16 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({params}) {
   // TODO get student information via params
-  return {
-    props: {
-      lrn: params.lrn
+  try {
+    return {
+      props: {
+        lrn: params.lrn,
+      }
+    }
+  } catch (e) {
+    console.error(e);
+    return {
+      notFound: true
     }
   }
 }
