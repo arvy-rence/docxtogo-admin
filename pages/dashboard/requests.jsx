95e02ed9c72fcd77a1cb7c54dacd0d188a5c444b
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2023-01-04 23:34:32 +0800

    [01/03/2023] / FINAL 40%! | Add refresh button on request page

--- pages/dashboard/requests.jsx
+++ pages/dashboard/requests.jsx
@@ -3,11 +3,15 @@ import React, {
   useState,
 } from "react";
 import {LoggedInNavbar} from "../../components/Navbar";
-import {Table, TextInput} from "flowbite-react";
+import {Table, TextInput, Button} from "flowbite-react";
 import RequestTableRow from "../../components/requests/TableRow";
 import {FaSearch} from "react-icons/fa";
 import axios from "../../server";
-import {Toaster} from "react-hot-toast";
+import {toast, Toaster} from "react-hot-toast";
+import {toastOptions} from "../../styles/modalOptions";
+import {delay} from "../../hooks/delay";
+import {useRouter} from "next/router";
+import {HiRefresh} from "react-icons/hi";
 
 /**
  * Page containing all requests that can be managed via the dropdown status. Uses the custom `RequestTableRow`
@@ -21,6 +25,7 @@ const Requests = ({requests}) => {
 
   const [search, setSearch] = useState("")
   const [filteredRequests, setFilteredRequests] = useState(requests)
+  const router = useRouter()
 
   useEffect(() => {
     const nameMatches = requests.filter((request) => request.name.toLowerCase().includes(search.toLowerCase()))
@@ -32,6 +37,16 @@ const Requests = ({requests}) => {
     setFilteredRequests(uniqueMatches)
   }, [search])
 
+  async function refreshHandler() {
+    toast('Checking for new data!', {
+      ...toastOptions,
+      duration: 2000,
+      icon: '🔄',
+    });
+    await delay(2000)
+    router.reload()
+  }
+
   return (
     <>
       <Toaster/>
@@ -41,15 +56,21 @@ const Requests = ({requests}) => {
           <h1 className="text-2xl font-work text-primary font-bold">
             Document Requests
           </h1>
-          <TextInput
-            id="search"
-            type="text"
-            icon={FaSearch}
-            placeholder="Search for anything"
-            onChange={(e) => setSearch(e.target.value)}
-            required={true}
-            className="font-work w-[20rem] h-[2.5rem] text-sm"
-          />
+          <div className="flex gap-4">
+            <TextInput
+              id="search"
+              type="text"
+              icon={FaSearch}
+              placeholder="Search for anything"
+              onChange={(e) => setSearch(e.target.value)}
+              required={true}
+              className="font-work w-[20rem] h-[2.5rem] text-sm"
+            />
+            <Button color="success" onClick={() => refreshHandler()} className="font-work uppercase font-bold">
+              <HiRefresh className="mr-2"/>
+              Refresh
+            </Button>
+          </div>
         </div>
         <div className="pb-5"></div>
         <Table striped={true}
